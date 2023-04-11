ee2984fb32f3f3db10c3e7322aa49a05abe4e4c3
lee-anthony7 <128553132+lee-anthony7@users.noreply.github.com>
2023-04-11 01:18:13 +0000

    setup 11apr2023
    setup 11apr2023

--- main.cs
+++ main.cs
@@ -1,9 +1,15 @@
+/*
+
+dotnet run --project /home/runner/Coding/main.csproj
+
+*/
+
 using System;
 
 namespace Coding;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    Console.WriteLine ("Hello World  is dotnet run --project /home/runner/Coding/main.csproj");
   }
 }
