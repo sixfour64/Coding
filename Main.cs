bf79c50bb1e0bdc9f8342c3034425301ec8b1e1c
lee-anthony7 <128553132+lee-anthony7@users.noreply.github.com>
2023-04-17 05:40:56 +0000

    move bulk of Scratch codings to Main.cs

---
+++ Main.cs
@@ -0,0 +1,99 @@
+/*
+
+dotnet --info && dotnet --list-runtimes && dotnet --list-sdks && dotnet --version && dotnet sdk check
+
+dotnet run --project /home/runner/Coding/Main.csproj
+
+*/
+
+using System;
+using System.Collections;
+using Humanizer;
+
+namespace Coding;
+
+class Program {
+  public static void Main (string[] args) {
+Console.WriteLine ($"{Environment.NewLine}Hello World  is dotnet run --project /home/runner/Coding/Main.csproj\n");
+
+var dayOfWeek = DateTime.Now.DayOfWeek;
+Console.WriteLine(dayOfWeek);
+
+string message = dayOfWeek switch 
+{
+    DayOfWeek.Monday     => "I've got a case of the Mondays",
+    DayOfWeek.Friday     => "It's FRI-YAY!!!",
+    DayOfWeek.Sunday     => "It's Sundee!!!",
+    _                    => "Default statement"
+};
+Console.WriteLine(message);
+
+
+// Parameterless constructor for class Person:
+var person = new Person();
+Console.WriteLine($"{Environment.NewLine}Person class:  Name: {person.name}, Age: {person.age}{Environment.NewLine}");
+// Output:  Name: unknown, Age: 0
+    
+
+// for Exceptions and writing your owqn too, see Fritz ep08  @  https://www.youtube.com/watch?v=Vj3GJTUaIaQ&list=PLdo4fOcmZ0oXv32dOd36UydQYLejKR61R&index=9
+decimal Divide(decimal arg1, decimal? arg2) {
+
+    try {
+        return arg1 / arg2.Value;
+      } catch (DivideByZeroException) {
+        Console.WriteLine("Division by zero is not allowed");
+        return 0;
+      } catch {
+        Console.WriteLine("You broke something else");
+        throw;
+      }
+
+Console.WriteLine(Divide(4, 0)+"  DivideByZeroException throws here\n.");
+  }
+
+
+var fs = default(FileStream);
+
+          try{
+    // Opens a text tile.
+    fs = new FileStream(@"./readme.md", FileMode.Open);
+    var sr = new StreamReader(fs);
+
+    // A value is read from the file and output to the console.
+    string? line = sr.ReadLine();
+    Console.WriteLine(line);
+  }
+
+      catch (FileNotFoundException e) {
+    Console.WriteLine($"[Data File Missing] {e}{Environment.NewLine}{e.StackTrace}{Environment.NewLine}");
+    throw new FileNotFoundException(@$"[./read.md not in replit root dir / c:\temp directory]{Environment.NewLine}.", e);
+  }
+
+      finally {
+    if (fs != null)
+        fs.Close();
+  }
+
+
+    
+} // end Main method
+}  // end Class
+
+
+public class Person
+{
+    public int age;
+    public string name = "unknown";
+  }
+
+/*
+  https://github.com/Humanizr/Humanizer 
+added to it as a package, 
+dotnet add package --help
+dotnet add [<PROJECT>] package <PACKAGE_NAME> [options]
+dotnet add [<PROJECT>] package Humanizer --version 2.14.1
+dotnet add Main.csproj package Humanizer --version 2.14.1
+see also: 
+  https://www.nuget.org/packages/Humanizer#readme-body-tab
+
+*/
--- Scratch.cs
+++ Scratch.cs
@@ -7,7 +7,7 @@ dotnet run --project /home/runner/Coding/Scratch.csproj
 */
 
 using System.Collections;
-using Humanizer;
+
 
 namespace Coding;
 
@@ -15,76 +15,12 @@ namespace Coding;
   {
     static void Main(string[] args)
     {
+ Console.WriteLine ("Hello World  is dotnet run --project /home/runner/Coding/Main.csproj");
 Console.WriteLine("Hello World!  Scratch me!!"); 
-var dayOfWeek = DateTime.Now.DayOfWeek;
-Console.WriteLine(dayOfWeek);
-
-string message = dayOfWeek switch 
-{
-    DayOfWeek.Monday     => "I've got a case of the Mondays",
-    DayOfWeek.Friday     => "It's FRI-YAY!!!",
-    DayOfWeek.Sunday     => "It's Sundee!!!",
-    _                    => "Default statement"
-};
-Console.WriteLine(message);
-
-      // Parameterless constructor for class Person:
-var person = new Person();
-Console.WriteLine($"Name: {person.name}, Age: {person.age}");
-// Output:  Name: unknown, Age: 0
-
-
-      // for Exceptions and writing your owqn too, see Fritz ep08  @  https://www.youtube.com/watch?v=Vj3GJTUaIaQ&list=PLdo4fOcmZ0oXv32dOd36UydQYLejKR61R&index=9
-decimal Divide(decimal arg1, decimal? arg2) {
-
-    try {
-        return arg1 / arg2.Value;
-    } catch (DivideByZeroException) {
-        Console.WriteLine("Division by zero is not allowed");
-        return 0;
-    } catch {
-        Console.WriteLine("You broke something else");
-        throw;
-    }
-}
-
-Console.WriteLine(Divide(4, 0)+"  DivideByZeroException throws here\n.");
-
-
-
-var fs = default(FileStream);
-try
-{
-    // Opens a text tile.
-    fs = new FileStream(@"./readm.md", FileMode.Open);
-    var sr = new StreamReader(fs);
-
-    // A value is read from the file and output to the console.
-    string? line = sr.ReadLine();
-    Console.WriteLine(line);
-}
-catch (FileNotFoundException e)
-{
-    Console.WriteLine($"[Data File Missing] {e}{Environment.NewLine}{e.StackTrace}{Environment.NewLine}");
-    throw new FileNotFoundException(@$"[./read.md not in replit root dir / c:\temp directory]{Environment.NewLine}.", e);
-}
-finally
-{
-    if (fs != null)
-        fs.Close();
-}
-
-      
-      
  }    // end method Main 
     
 }  // end class
-    
-public class Person
-{
-    public int age;
-    public string name = "unknown";
-}
+
 
 
 /*
