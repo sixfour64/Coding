273e5ebdc8a6efa7fea70329a239d5029f04d631
lee-anthony7 <128553132+lee-anthony7@users.noreply.github.com>
2023-04-23 13:33:05 +1000

    Update W3Schools02.cs

--- W3Schools02.cs
+++ W3Schools02.cs
@@ -188,6 +188,10 @@ you can pass an array of args as inputs to the Main() method when you start the 
 
 
 You can define a custom value type via struct:  public struct Point { public int X; public int Y; }
+Structs are Values, not references.  Structs are not inheritable.
+Structs cannot be subclassed.
+Each instance from a struct does not require instantiation of an object on the heap.
+A struct instance cannot be null. The default value for a struct is an empty instance, with all fields empty (set to their default values).
 
 
 Delegates
